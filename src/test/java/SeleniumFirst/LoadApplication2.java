6702dc3a8f18b8a402fb53cfac99fd6e175672a2
Palaniraj17 <Palanibala@LAPTOP-BD96GJ6G>
2025-02-08 23:26:41 +0530

    test commit

---
+++ src/test/java/SeleniumFirst/LoadApplication2.java
@@ -0,0 +1,21 @@
+package SeleniumFirst;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoadApplication2 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String CurrentURL = driver.getCurrentUrl();
+		System.out.println("The current URL of the page is "+CurrentURL);
+		System.out.println("The first proj");
+		driver.navigate().refresh();
+		driver.close();
+	}
+}
